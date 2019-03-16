5f1959b901583de28c50e729ef8c61623eac520c
xWReXx <wrex1285@gmail.com>
2019-03-15 22:48:19 -0600

    tank player controller blueprint now has access C++ class

---
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank) 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player controller not posessing tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player controller possesing %s"), *ControlledTank->GetName());
+	}
+}
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
