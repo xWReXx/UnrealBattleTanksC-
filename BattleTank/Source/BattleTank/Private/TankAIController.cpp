9e5b4e9b23fb0a4ce5c684517fc6f4fa87f8a3b3
xWReXx <wrex1285@gmail.com>
2019-03-18 16:59:04 -0600

    AI controller class created, and logging out controlled tank.

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI controller not posessing tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI controller possesing %s"), *ControlledTank->GetName());
+	}
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
