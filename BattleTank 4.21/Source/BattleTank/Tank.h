de4c6610e7e04811aa02537b1ce16f57989825d9
xWReXx <wrex1285@gmail.com>
2019-03-24 22:51:44 -0600

    Created aiming component and logging aim location from the component

--- BattleTank 4.21/Source/BattleTank/Tank.h
+++ BattleTank 4.21/Source/BattleTank/Tank.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "TankAimingComponent.h"
 #include "Tank.generated.h"
 
 UCLASS()
@@ -14,13 +15,18 @@ class BATTLETANK_API ATank : public APawn
 public:
 	// Sets default values for this pawn's properties
 	void AimAt(FVector HitLocation);
-	ATank();
+	
 
 protected:
+	
+	UTankAimingComponent* TankAimingComponent = nullptr;
+
+private:
+	ATank();
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
