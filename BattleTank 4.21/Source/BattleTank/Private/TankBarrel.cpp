9b69376e0d0119c6063ebbb6822513421a947621
xWReXx <wrex1285@gmail.com>
2019-03-26 22:41:21 -0600

    Reworked execution flow for tank actors

--- BattleTank 4.21/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank 4.21/Source/BattleTank/Private/TankBarrel.cpp
@@ -7,5 +7,4 @@ void UTankBarrel::Elevate(float DegreesPerSecond)
 {
 	// Move the barrel the right amount this frame
 	// Given a max elevation speed, and the frame time
-	UE_LOG(LogTemp, Warning, TEXT("Barrel->Elevate() called at speed %f"), DegreesPerSecond);
 }
